63fe7d935334ecf29f83fef3fb726e880dfff762
stillerndenacht <vanuit@gmx.de>
2024-06-24 12:27:33 +0200

    inital commit

---
+++ php/uebungsformular.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hallo";
+echo "<h1>Die Serverseite von uebungsformular</h1>";
+echo $_GET['name'] . '<br>';
+echo $_GET['vn'] . '<br>';
+echo $_GET['gebname'] . '<br>';
+echo $_GET['geschlecht'] . '<br>';
+echo $_GET['gebdate'] . '<br>';
+echo $_GET['anschrift'] . '<br>';
+phpinfo();
---
+++ php/verarbeitung.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hallo";
+echo "<h1>Hallo geht auch mit html-Tags</h1>";
+ echo $_GET['name'] . '<br>';
+// echo $_GET['date'] . '<br>';
+phpinfo();
+
+?>
