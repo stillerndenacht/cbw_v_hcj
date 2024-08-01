f36a060ccc2935c6f58ce3550bc26f0f536747b3
stillerndenacht <vanuit@gmx.de>
2024-08-01 15:55:30 +0200

    die medalapp angefangen

---
+++ react/demo/medalapp/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
+
+/* Aufgabe: Medaillenspiegel in REACT umsetzen mit Objektliteralen und als Liste
+03 Listing 27 als Vorlage */
---
+++ react/demo/medalapp/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header(){
+    return(
+        <header>
+            <h1>Meine Medalapp</h1>
+        </header>
+    )
+}
+export default Header;
+// import in App.jsx
