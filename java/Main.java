7f622097b929148cf809cc02e715ac54f5149856
stillerndenacht <vanuit@gmx.de>
2024-09-24 16:48:38 +0200

    anfang java und intellij

--- java/Main.java
+++ java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Taschenrechner taschenrechner = new Taschenrechner(5,5);
+        System.out.print(taschenrechner);
         
 
 
