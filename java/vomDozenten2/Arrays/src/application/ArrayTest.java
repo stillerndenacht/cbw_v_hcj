3bfa6009d8993e2191bfca23181206b31a8bccaf
stillerndenacht <61967922+stillerndenacht@users.noreply.github.com>
2024-10-13 21:55:07 +0200

    arrayThings und Dozenten Zeug

---
+++ java/vomDozenten2/Arrays/src/application/ArrayTest.java
@@ -0,0 +1,57 @@
+package application;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int x;
+		int feld[], test;
+		test = 7;
+		
+		feld = new int[0];
+		
+		int intArray1[] = new int[5];
+		
+		for(int zahl : intArray1) {
+			System.out.println(zahl);
+		}
+		
+		for(int index = 0; index < intArray1.length; ++index) {
+			intArray1[index] = (int) (Math.random() * 100) + 1;
+		}
+		
+		for(int zahl : intArray1) {
+			System.out.println(zahl);
+		}
+		
+		Rechteck[] rechtecke = new Rechteck[5];
+		for(int index = 0; index < rechtecke.length; ++index) {
+			rechtecke[index] = new Rechteck(Math.random() * 200, Math.random() * 100);
+		}
+		
+		for(Rechteck r : rechtecke) {
+			System.out.println(r);
+		}
+		
+		int[] intArray2 = {23, 56, 78, 34, 100};
+		int[] intArray3 = new int[]{23, 56, 78, 34, 100};
+		
+		for(int z : intArray2) {
+			System.out.print(z + "\t");
+		}
+		System.out.println();
+		for(int z : intArray3) {
+			System.out.print(z + "\t");
+		}
+		
+		Rechteck[] rechtecke2 = {new Rechteck(), new Rechteck(3, 7)};
+		Rechteck[] rechtecke3 = new Rechteck[] {new Rechteck(), new Rechteck(3, 7)};
+		for(Rechteck r : rechtecke2) {
+			System.out.println(r);
+		}
+		
+		for(Rechteck r : rechtecke3) {
+			System.out.println(r);
+		}
+	}
+
+}
---
+++ java/vomDozenten2/Arrays/src/application/ZweitdimensionaleArrays.java
@@ -0,0 +1,64 @@
+package application;
+
+public class ZweitdimensionaleArrays {
+	
+	public static void main(String[] args) {
+		// zweidimensional
+		int[][] array1 = new int[3][5];
+		int array2[][] = new int[3][5];
+		int[] array3[] = new int[3][5];
+		
+		int[][] asymetrisch = new int[4][];
+		
+		for(Object z : asymetrisch) {
+			System.out.println(z);
+		}
+		
+		for(int index = 0; index < asymetrisch.length; ++ index) {
+			asymetrisch[index] = new int[(int) (Math.random() * 10 + 1)];
+		}
+		
+		for(int index1 = 0; index1 < asymetrisch.length; ++index1) {
+			for(int index2 = 0; index2 < asymetrisch[index1].length; ++index2) {
+				asymetrisch[index1][index2] = (int)(Math.random() * 100 + 1);
+			}
+		}
+		
+		for(int[] zeile  : asymetrisch) {
+			for(int zahl : zeile) {
+				System.out.print(zahl + "\t");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n===================================\n");
+		
+		array1[0][2] = 45;
+		
+		// dreidimensional
+		int[][][] array4 = new int[3][2][2];
+		int[][] array5[] = new int[3][2][2];
+		int[] array6[][] = new int[3][2][2];
+		int array7[][][] = new int[3][2][2];
+		
+		
+		for(int index1 = 0; index1 < array1.length; ++index1) {
+			for(int index2 = 0; index2 < array1[index1].length; ++index2) {
+				array1[index1][index2] = (int)(Math.random() * 100 + 1);
+			}
+		}
+		
+		for(int[] zeile  : array1) {
+			for(int zahl : zeile) {
+				System.out.print(zahl + "\t");
+			}
+			System.out.println();
+		}
+		
+		int[][] array8 = {{2, 5, 7, 6, 9}, {1, 8, 4}, {45, 78}, {23}};		
+		int[][][] array9 = {{{2, 5, 7, 6, 9}, {1, 8, 4}}, {{45, 78}, {23}}};	
+		
+		System.out.println(array9[1][2][1]);
+	}
+
+}
