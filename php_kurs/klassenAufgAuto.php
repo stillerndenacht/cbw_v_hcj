23d0751420814d75858812e20786400eb14f7350
stillerndenacht <61967922+stillerndenacht@users.noreply.github.com>
2025-02-18 21:54:05 +0100

    klassenAufg Autovermietung

--- php_kurs/klassenAufgAuto.php
+++ php_kurs/klassenAufgAuto.php
@@ -9,8 +9,28 @@ abstract class Car
         $this->kennzeichen = $kennzeichen;
         $this->fahrgestellnr = $fahrgestellnr;
     }
-}
 
+    public function getKennzeichen()
+    {
+        return $this->kennzeichen;
+    }
+
+    public function setKennzeichen($kennzeichen)
+    {
+        $this->kennzeichen = $kennzeichen;
+    }
+
+    public function getFahrgestellnr()
+    {
+        return $this->fahrgestellnr;
+    }
+
+    public function setFahrgestellnr($fahrgestellnr)
+    {
+        $this->fahrgestellnr = $fahrgestellnr;
+    }
+}
+# ----------- PKW -------------------
 class PKW extends Car
 {
     public $sitzpl = 0;
@@ -19,8 +39,18 @@ class PKW extends Car
         parent::__construct($kennzeichen, $fahrgestellnr);
         $this->sitzpl = $sitzpl;
     }
-}
 
+    public function getSitzpl()
+    {
+        return $this->sitzpl;
+    }
+
+    public function setSitzpl($sitzpl)
+    {
+        $this->sitzpl = $sitzpl;
+    }
+}
+# ----------- LKW ---------------------
 class LKW extends Car
 {
     public $sitzpl = 0;
@@ -31,4 +61,65 @@ class LKW extends Car
         $this->sitzpl = $sitzpl;
         $this->zuladung = $zuladung;
     }
+
+    public function getSitzpl()
+    {
+        return $this->sitzpl;
+    }
+
+    public function setSitzpl($sitzpl)
+    {
+        $this->sitzpl = $sitzpl;
+    }
+
+    public function getZuladung()
+    {
+        return $this->zuladung;
+    }
+
+    public function setZuladung($zuladung)
+    {
+        $this->zuladung = $zuladung;
+    }
 }
+# ----------- Unternehmen -----------
+class Company
+{
+    public $company = 'NoName';
+    public $cars = [];
+
+    function __construct($company)
+    {
+        $this->company = $company;
+    }
+    function setCars(Car ...$cars)
+    {
+        foreach ($cars as $car) {
+            $this->cars[] = $car;
+        }
+    }
+    function getCarlist(){
+        foreach($this->cars as $car){
+            $zuladung = "k.A.";
+            if($car instanceof LKW){
+                $zuladung = $car->zuladung;
+            }
+            echo "<hr>Kennzeichen : ".$car->kennzeichen." | Fahrgestellnummer : ".$car->getFahrgestellnr()." | Sitzplätze : ".$car->getSitzpl()." | Zuladung : ".$zuladung;
+        }
+        echo "<hr>";
+    }
+    function rentCar($car, $person, $time = 1){
+        $kennzeichen = $car->getKennzeichen();
+        echo $person." mietet ".$kennzeichen." für ".$time." Stunden von der Firma ".$this->company.".";
+    }
+}
+
+$vw = new PKW('vw-01-01', '0101', 5);
+$kia = new PKW('kia-02-02', '0202', 5);
+$iveco = new LKW('iveco-03-03', '0303', 3, 3500);
+echo $vw->getFahrgestellnr();
+
+$sixt = new Company('Sixt');
+$sixt->setCars($vw, $kia,$iveco);
+$sixt->getCarlist();
+$sixt->rentCar($vw, 'Ferdinand', 24);
